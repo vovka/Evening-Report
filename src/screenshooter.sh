fdd935d4af80064d79fa511fb323508e6e540717
vovka <sch.icq@gmail.com>
2010-06-14 12:03:24 +0300

    readme updated with instructions to convert utility

--- src/screenshooter.sh
+++ src/screenshooter.sh
@@ -7,6 +7,6 @@ xwd_file=/tmp/$file_name.xwd
 target_file=$path$file_name.$file_extension
 
 xwd -display :0 -root > $xwd_file
-/usr/local/bin/convert $xwd_file $target_file
+/usr/bin/convert $xwd_file $target_file
 rm -f $xwd_file
 
