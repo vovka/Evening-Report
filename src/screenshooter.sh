93a3aaa5cbf29bc47a0c92ef690aab35c6fa6eb0
Volodymyr Shcherbyna <scherbina.v@gmail.com>
2019-07-24 23:43:37 +0300

    Save screenshots to separate daily dirs

--- src/screenshooter.sh
+++ src/screenshooter.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-file_name="`date +%Y-%m-%d_%H:%M:%S`"
+dir_name="`date +%Y-%m-%d`"
+file_name="`date +%H:%M:%S`"
 file_extension="png"
-path=/home/vovka/Зображення/screenshots/daily_screenshots/
+path=/home/vovka/Зображення/screenshots/daily_screenshots/$dir_name/
 xwd_file=/tmp/$file_name.xwd
 target_file=$path$file_name.$file_extension
 
 xwd -display :0 -root > $xwd_file
+mkdir -p $path
 /usr/bin/convert $xwd_file $target_file
 rm -f $xwd_file
-
